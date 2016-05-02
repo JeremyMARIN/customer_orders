0a36ef591a23195a594928e9d7291995e397cfbd
Jeremy MARIN <jgm438@nyu.edu>
2016-05-02 16:42:25 -0400

    Add a 'Sold Out' sign when a game is out of stock

---
+++ include/sold_out.php
@@ -0,0 +1,60 @@
+<?php
+
+function rotatetransparentimg($imageSource, $angle) {
+	$transp = imagecolorallocatealpha($imageSource, 0, 0, 0, 127);
+
+	imagealphablending($result, false);
+	imagesavealpha($result, true);
+
+	imagefill($imageSource, 0, 0, $transp);
+
+	$result = imagerotate($imageSource, $angle, $transp);
+
+	imagealphablending($result, false);
+	imagesavealpha($result, true);
+
+	return $result;
+}
+
+// Create a 1200*600 image
+$im = imagecreatetruecolor(1200, 600);
+
+// Create some colors
+$red = imagecolorallocate($im, 255, 0, 0);
+$white = imagecolorallocate($im, 255, 255, 255);
+$darkRed = imagecolorallocate($im, 100, 100, 100);
+
+
+// Draw the frame:
+imagefilledrectangle($im, 0, 0, 1200, 600, $white);
+imagefilledrectangle($im, 10, 130, 1190, 400, $red);
+imagefilledrectangle($im, 30, 150, 1170, 380, $white);
+
+
+// Draw the text:
+// The text to draw
+$text = "Sold out!";
+
+// Replace path by your own font path
+$font = "./arial.ttf";
+
+// Add some shadow to the text
+imagettftext($im, 190, 0, 100, 355, $darkRed, $font, $text);
+
+// Add the text
+imagettftext($im, 190, 0, 100, 350, $red, $font, $text);
+
+
+imagecolortransparent($im, $white);
+
+
+
+$im = rotatetransparentimg($im, 20);
+
+
+// Output and free memory
+header("Content-type: image/png");
+imagepng($im);
+imagedestroy($im);
+
+?>
--- js/customer_order.js
+++ js/customer_order.js
@@ -41,7 +41,7 @@ function processListData(data, listContent) {
 			if (data[i].stock == 0)
 				line += "gray";
 			line += "\" style=\"background-image: url('../inventory/img/" + data[i].file + "');\"></div>";
-			line += "</div><div class=\"information radius\" data-id=\"" + data[i].id + "\">";
+			line += "</div><div class=\"information radius\" data-id=\"" + data[i].id + "\" data-stock=\"" + data[i].stock + "\" >";
 			line += "<h4 class=\"title\">" + data[i].title + "</h4>";
 			line += "<h5 class=\"price\">$" + data[i].price + "</h5>";
 			line += "<input type=\"hidden\" name=\"" + data[i].id + "\" value=\"0\"></input>";
@@ -63,25 +63,27 @@ function addOrderButtons() {
 	var informationDivs = $(".information");
 	// console.log(informationDivs);
 	for (var i = 0, max = informationDivs.length; i < max; i++) {
-		var button = $("<button>", {
-			text: "Order",
-			type: "button",
-			class: "radius green grow",
-			click: function() {
-				if ($(this).hasClass("green")) {
-					console.log("Ordering product of id: " + $(this).parent().data("id"));
-					$(this).removeClass("green").addClass("red");
-					$(this).text("In Cart");
-					$(this).parent().find("input").val(1);
-				} else {
-					console.log("Unordering product of id: " + $(this).parent().data("id"));
-					$(this).removeClass("red").addClass("green");
-					$(this).text("Order");
-					$(this).parent().find("input").val(0);
+		if ($(informationDivs[i]).data("stock") > 0) {
+			var button = $("<button>", {
+				text: "Order",
+				type: "button",
+				class: "radius green grow",
+				click: function() {
+					if ($(this).hasClass("green")) {
+						console.log("Ordering product of id: " + $(this).parent().data("id"));
+						$(this).removeClass("green").addClass("red");
+						$(this).text("In Cart");
+						$(this).parent().find("input").val(1);
+					} else {
+						console.log("Unordering product of id: " + $(this).parent().data("id"));
+						$(this).removeClass("red").addClass("green");
+						$(this).text("Order");
+						$(this).parent().find("input").val(0);
+					}
 				}
-			}
-		});
-		console.log(informationDivs[i]);
-		$(informationDivs[i]).append(button);
+			});
+			console.log(informationDivs[i]);
+			$(informationDivs[i]).append(button);
+		}
 	}
 }
